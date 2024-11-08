da629c7c8ecc566f7bf991ad8409f2feedeb7876
yohannes <yohannesguesh01@gmail.com>
2024-11-08 03:23:00 +0300

    test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+build: {
+    outDir: 'dist'  // Make sure this is set to 'dist' (default)
+  }
 })
